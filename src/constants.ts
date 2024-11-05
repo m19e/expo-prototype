f16bacc357ef683de3be3da4a82d5fe28e6b1213
m19e <m19e.dev@gmail.com>
2024-11-05 16:11:06 +0900

    Add consts for members

---
+++ src/constants.ts
@@ -0,0 +1,61 @@
+export const MEMBER_MAP = {
+    "Starrykarasu": {
+        id: "Starrykarasu",
+        name: "ほしの",
+        url: {
+            pixiv: "",
+        },
+    },
+    "hakkadr": {
+        id: "hakkadr",
+        name: "はっか飴",
+        url: {
+            pixiv: "",
+        },
+    },
+    "hataori_ryo": {
+        id: "hataori_ryo",
+        name: "はたおり",
+        url: {
+            pixiv: "",
+        },
+    },
+    "o2_hk5": {
+        id: "o2_hk5",
+        name: "りっか",
+        url: {
+            pixiv: "",
+        },
+    },
+    "cocoa_mouso": {
+        id: "cocoa_mouso",
+        name: "ココア",
+        url: {
+            pixiv: "",
+        },
+    },
+    "gene_lt4147": {
+        id: "gene_lt4147",
+        name: "ジェネ/楸燈樹",
+        url: {
+            pixiv: "",
+        },
+    },
+    "mikamihoP": {
+        id: "mikamihoP",
+        name: "ほり。|軍鶏",
+        url: {
+            pixiv: "",
+        },
+    },
+} as const;
+
+export const MEMBER_ID_LIST = [
+    "Starrykarasu",
+    "hakkadr",
+    "hataori_ryo",
+    "o2_hk5",
+    "cocoa_mouso",
+    "gene_lt4147",
+    "mikamihoP",
+] as const;
