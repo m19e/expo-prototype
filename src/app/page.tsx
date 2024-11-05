aeedb2bbff4d8aece2cc08eb59f07550961f588c
m19e <m19e.dev@gmail.com>
2024-11-05 16:16:18 +0900

    feat: show book info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,87 @@
+import React from "react";
 import Image from "next/image";
+import { MEMBER_ID_LIST, MEMBER_MAP } from "@/constants";
 
-export default function Home() {
+export default async function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="grid items-center justify-items-center font-[family-name:var(--font-geist-sans)]">
+      <header className="bg-gray-100 w-full relative min-h-screen">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
+          src="/cover_room.png"
+          alt="cover room"
+          fill
+          style={{ objectFit: "cover" }}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <div className="expo-title bg-gradient-to-r from-indigo-500 to-pink-200 pt-1 pr-1">
+          <span className="text-6xl text-white font-extralight bg-gradient-to-b from-white via-white to-transparent inline-block text-transparent bg-clip-text">
+            Twinkle Happy Sharing!
+          </span>
+        </div>
+      </header>
+      <main className="flex flex-col gap-8 py-8 px-16 items-center bg-blue-100 w-full">
+        <p className="text-6xl text-blue-400">Information</p>
+        <div
+          className="relative w-full "
+          style={{
+            aspectRatio: "1 / 1.4",
+          }}
+        >
+          <Image
+            className="shadow-md"
+            src="/cover_trim.jpg"
+            alt="cover"
+            fill
+            style={{ objectFit: "cover" }}
+            priority
+          />
+        </div>
+        <div className="grid gap-2 w-80">
+          <div className="flex flex-col">
+            <p>タイトル</p>
+            <p className="text-right">Twinkle Happy Sharing! 2nd SEASON</p>
+          </div>
+          <div className="flex flex-col">
+            <p>仕様</p>
+            <p className="text-right">仕様を入力</p>
+          </div>
+          <div className="flex flex-col">
+            <p>頒布イベント</p>
+            <p className="text-right">イベント情報を入力</p>
+          </div>
+          <div className="flex flex-col">
+            <p>会場頒布価格</p>
+            <p className="text-right">価格を入力</p>
+          </div>
+        </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <p className="text-6xl text-blue-400">Members</p>
+        <div className="grid grid-cols-1 sm:grid-cols-3  items-center gap-2">
+          {MEMBER_ID_LIST.map((memberId) => (
+            <div
+              key={memberId}
+              className="flex flex-col items-center bg-yellow-100 rounded-md  overflow-hidden shadow-md"
+            >
+              <div
+                className="relative min-w-40"
+                style={{
+                  aspectRatio: "1 / 1",
+                }}
+              >
+                <Image
+                  src={`/avatar/${memberId}.jpg`}
+                  alt="cover"
+                  fill
+                  style={{ objectFit: "cover" }}
+                  priority
+                />
+              </div>
+              <p className="text-sm">{MEMBER_MAP[memberId].name}</p>
+            </div>
+          ))}
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      {/* <footer className="bg-gray-100">FOOTER</footer> */}
     </div>
   );
 }
