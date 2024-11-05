aa39c01ba1456e0b6f6ed88b8b4f0a81d375405b
m19e <m19e.dev@gmail.com>
2024-11-05 19:50:53 +0900

    Fix layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,50 +13,60 @@ export default async function Home() {
           style={{ objectFit: "cover" }}
           priority
         />
-        <div className="expo-title bg-gradient-to-r from-indigo-500 to-pink-200 pt-1 pr-1">
+        <div className="expo-title bg-gradient-to-r from-indigo-400  to-pink-200 pt-1 pr-1">
           <span className="text-6xl text-white font-extralight bg-gradient-to-b from-white via-white to-transparent inline-block text-transparent bg-clip-text">
             Twinkle Happy Sharing!
           </span>
         </div>
       </header>
-      <main className="flex flex-col gap-8 py-8 px-16 items-center bg-blue-100 w-full">
+      <main className="flex flex-col gap-8 p-8 items-center bg-blue-100 w-full">
         <p className="text-6xl text-blue-400">Information</p>
-        <div
-          className="relative w-full "
-          style={{
-            aspectRatio: "1 / 1.4",
-          }}
-        >
-          <Image
-            className="shadow-md"
-            src="/cover_trim.jpg"
-            alt="cover"
-            fill
-            style={{ objectFit: "cover" }}
-            priority
-          />
-        </div>
-        <div className="grid gap-2 w-80">
-          <div className="flex flex-col">
-            <p>タイトル</p>
-            <p className="text-right">Twinkle Happy Sharing! 2nd SEASON</p>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <div
+            className="relative w-full self-stretch md:self-auto"
+            style={{
+              aspectRatio: "1 / 1.4",
+            }}
+          >
+            <Image
+              className="shadow-md"
+              src="/cover_trim.jpg"
+              alt="cover"
+              fill
+              style={{ objectFit: "cover" }}
+              priority
+            />
           </div>
-          <div className="flex flex-col">
-            <p>仕様</p>
-            <p className="text-right">仕様を入力</p>
-          </div>
-          <div className="flex flex-col">
-            <p>頒布イベント</p>
-            <p className="text-right">イベント情報を入力</p>
-          </div>
-          <div className="flex flex-col">
-            <p>会場頒布価格</p>
-            <p className="text-right">価格を入力</p>
+          <div className="grid gap-2 w-80">
+            <div className="flex flex-col">
+              <p>タイトル</p>
+              <p className="text-right">Twinkle Happy Sharing! 2nd SEASON</p>
+            </div>
+            <div className="flex flex-col">
+              <p>仕様</p>
+              <p className="text-right">A5 本文xxP</p>
+            </div>
+            <div className="flex flex-col">
+              <p>頒布イベント</p>
+              <p className="text-right">
+                アイマスEXPO
+              </p>
+            </div>
+            <div className="flex flex-col">
+              <p>頒布サークル</p>
+              <p className="text-right">
+                1日目 タ-1b HoShiKaRaSu
+              </p>
+            </div>
+            <div className="flex flex-col">
+              <p>会場頒布価格</p>
+              <p className="text-right">xxxx円</p>
+            </div>
           </div>
         </div>
 
         <p className="text-6xl text-blue-400">Members</p>
-        <div className="grid grid-cols-1 sm:grid-cols-3  items-center gap-2">
+        <div className="grid grid-cols-1 sm:grid-cols-2 items-center gap-2">
           {MEMBER_ID_LIST.map((memberId) => (
             <div
               key={memberId}
