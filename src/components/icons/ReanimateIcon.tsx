f39e581279bb21cfad36651c166785ad93e1938d
m19e <m19e.dev@gmail.com>
2024-11-12 21:24:51 +0900

    Add wrapper for reanimate svg icon

---
+++ src/components/icons/ReanimateIcon.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useIntersectionObserver } from "@/hooks/useIntersectionObserver";
+import { ReactNode } from "react";
+
+type Props = {
+    className?: string;
+    children: ReactNode;
+};
+
+export const ReanimateIcon = (
+    { className = "", children }: Readonly<Props>,
+) => {
+    const [ref] = useIntersectionObserver({
+        onChange: (isIntersecting, entry) => {
+            isIntersecting &&
+                (entry.target as SVGSVGElement).setCurrentTime(0);
+        },
+    });
+
+    return (
+        <svg
+            ref={ref}
+            className={className}
+            xmlns="http://www.w3.org/2000/svg"
+            width="128"
+            height="128"
+            viewBox="0 0 24 24"
+        >
+            {children}
+        </svg>
+    );
+};
