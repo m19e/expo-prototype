651c8e67ac2a8007c37f35ae35df7c92be027caa
m19e <m19e.dev@gmail.com>
2024-11-15 14:44:12 +0900

    Add member-data type

--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,10 @@ import { CONTACT_ID_LIST, MEMBER_ID_LIST } from "@/constants";
 export type MemberID = typeof MEMBER_ID_LIST[number];
 
 export type ContactID = typeof CONTACT_ID_LIST[number];
+
+export type MemberData = {
+    id: string;
+    name: string;
+    role?: string | undefined;
+    github?: string | undefined;
+};
