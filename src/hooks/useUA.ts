cef83081e679e7205639c01490ee515a35535a63
m19e <m19e.dev@gmail.com>
2024-11-26 17:57:27 +0900

    Fix ios state initial value

--- src/hooks/useUA.ts
+++ src/hooks/useUA.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 export const useUA = () => {
-    const [iOS, setIOS] = useState(true);
+    const [iOS, setIOS] = useState(false);
     useEffect(() => {
         setIOS(
             /(iphone|ipad|ipod|mac)/.test(navigator.userAgent.toLowerCase()),
