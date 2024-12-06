e3cda9b6c1e6dd48e8cc424e9f90515ad2e7d98c
m19e <m19e.dev@gmail.com>
2024-12-06 17:40:09 +0900

    Show sample image in dialog

--- src/components/SampleDialog.tsx
+++ src/components/SampleDialog.tsx
@@ -30,11 +30,17 @@ export const SampleDialog = ({ id }: Readonly<Props>) => {
             </div>
 
             <dialog id={modalId} className="modal">
-                <div className="modal-box">
-                    <h3 className="font-bold text-lg">Hello!</h3>
-                    <p className="py-4">
-                        Press ESC key or click outside to close
-                    </p>
+                <div className="modal-box bg-gradient-to-br from-blue-100 via-pink-100 to-yellow-50">
+                    <div className="relative max-h-[calc(100vh-8rem)] aspect-[9/16] mx-auto">
+                        <Image
+                            className="border-2"
+                            src={`/sample/${id}.png`}
+                            alt="cover"
+                            fill
+                            style={{ objectFit: "cover" }}
+                            priority
+                        />
+                    </div>
                 </div>
                 <form method="dialog" className="modal-backdrop">
                     <button>close</button>
