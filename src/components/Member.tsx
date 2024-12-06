c2f281d3af4a1455c728608aaa303c153db61d8c
m19e <m19e.dev@gmail.com>
2024-12-06 16:38:51 +0900

    Replace member avatar by sample-dialog

--- src/components/Member.tsx
+++ src/components/Member.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { MEMBER_MAP } from "@/constants";
 import { MemberID } from "@/types";
 import { TwitterIconForA11y } from "@/components/icons/forAccessibility/Twitter";
+import { SampleDialog } from "@/components/SampleDialog";
 
 type Props = {
     id: MemberID;
@@ -10,15 +11,7 @@ type Props = {
 export const Member = ({ id }: Readonly<Props>) => {
     return (
         <div className="flex flex-col items-center">
-            <div className="relative min-w-40 aspect-square overflow-hidden rounded-md shadow-md">
-                <Image
-                    src={`/avatar/${id}.jpg`}
-                    alt="cover"
-                    fill
-                    style={{ objectFit: "cover" }}
-                    priority
-                />
-            </div>
+            <SampleDialog id={id} />
             <div className="w-full flex items-center justify-between py-2">
                 <p className="">{MEMBER_MAP[id].name}</p>
                 <a
